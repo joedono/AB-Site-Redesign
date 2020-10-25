f884f1e692adf35420c7670f07f2c4b5b88cbbdc
joedono <fjzusi@gmail.com>
2020-10-24 20:43:19 -0400

    Built most of the programming schedule page

--- js/main.js
+++ js/main.js
@@ -55,6 +55,82 @@ function scrollToTop() {
   window.scrollTo(0, 0);
 }
 
+var categories = {
+  1: "#FFCC66",
+  2: "#FFCC99",
+  3: "#F7921B",
+  4: "#FF9966",
+  5: "#00FF00",
+  6: "#00FF99",
+  7: "#3399FF",
+  8: "#00FFFF",
+  9: "#E8D37E",
+  10: "#669900",
+  11: "#9900FF",
+  13: "#FF00FF",
+  17: "#669999",
+  18: "#436EEE",
+  19: "#CCCCCC",
+  21: "#FFFF00",
+  22: "#0099CC",
+  23: "#CC6699",
+  24: "#3366CC",
+  25: "#9933FF",
+  27: "#FF0000",
+  28: "#CCFFCC",
+  29: "#CCFF00",
+  30: "#996633",
+  31: "#CCCCCC",
+  32: "#CCFF66",
+  33: "#669966",
+  34: "#CC0066",
+  35: "#AA99FF",
+  36: "#EEAEEE",
+  37: "#FF6699",
+  38: "#CC99FF",
+  39: "#CCCCFF",
+  40: "#CC9900"
+};
+
+function refreshCategoryHighlight() {
+  var checkedCategoryIds = [];
+  var checkedCategories = $(".category-check:checked");
+  checkedCategories.each(function(index, item) {
+    checkedCategoryIds.push($(item).data("value"));
+  });
+
+  $(".schedule-event").each(function(index, item) {
+    setEventColor(this, checkedCategoryIds);
+  });
+}
+
+function setEventColor(element, checkedCategoryIds) {
+  element = $(element);
+  var style = "";
+  var colors = Array();
+  var classes = element.attr("class").split(" ");
+
+  if (checkedCategoryIds.length == 0) {
+    checkedCategoryIds = Object.keys(categories);
+  }
+
+  checkedCategoryIds.forEach(function(checkedCategoryId, index) {
+    if (classes.includes("fn-event-category-" + checkedCategoryId)) {
+      colors.push(categories[checkedCategoryId]);
+    }
+  });
+
+  if (colors.length == 1) {
+    style += colors[0];
+  } else if (colors.length > 1) {
+    style += "linear-gradient(to bottom, ";
+    style += colors.join(",");
+    style += ")";
+  }
+
+  element.css("background", style);
+}
+
 $(function() {
   $("input").change(function() {
     $(this).parents(".has-error").removeClass("has-error").removeClass("has-feedback");
@@ -98,4 +174,6 @@ $(function() {
   });
 
   navRemember();
+
+  refreshCategoryHighlight();
 });
